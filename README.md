7b62fa4d3a553f873b0d276baea4b8731dc63260
jupsimar <82681171+jupsimar@users.noreply.github.com>
2023-10-11 14:07:42 +0000

    All Sumission

--- README.md
+++ README.md
@@ -1,54 +1,61 @@
 # Overview
-This is a basic Replit template for building a [Steamship](https://steamship.com) package for prompt-based text generation.
+This is a Replit example of a Question Answering app built with [LangChain](https://langchain.readthedocs.io/en/latest/) (🦜️🔗) and deployed on [Steamship](https://steamship.com).
+
+Steamship is the fastest way to build, ship, and use full-lifecycle language AI.
+
+By deploying on Steamship, LangChain developers get:
+
+- Production-ready API endpoint(s)
+- Horizontal scaling across dependencies / backends
+- Persistent storage of app state (including caches)
+- Built-in support for Authn/z 
+- Multi-tenancy support
+- Seamless integration with other Steamship skills (ex: audio transcription) 
+- Usage Metrics and Logging
+- And much more...
+
+The GitHub repo for the LangChain adapters for Steamship is: https://github.com/steamship-core/steamship-langchain
 
 ## Quick Start
 
-To get started, Click `Use Template` from the [template page](https://replit.com/@steamship/Steamship-Prompt-App-Template?v=1) in Replit.
-
-To run it:
+To run it locally:
 1. Get a Steamship API Key (Visit: [https://steamship.com/account/api](https://www.steamship.com/account/api)). If you do not
    already have a Steamship account, you will need to create one.
 1. Copy this key to a [Replit Secret](https://docs.replit.com/programming-ide/storing-sensitive-information-environment-variables) named `STEAMSHIP_API_KEY`.
 1. Click the green `Run` button at the top of the window (or open a new Shell and type `python3 api.py`).
 
-# Prompt-based Text Generation
+If you want to get a production API endpoint for your app, type `ship deploy` in a Shell.
 
-This template provides a simple mechanism to generate text based on a supplied prompt. The prompt can ask for any kind of text to be generated, including poems, letters, Twitter responses, LinkedIn posts, jokes, etc. The prompt can be customized based on user input, allowing your users to generate personalized text.
+# Question Answering API
 
-## Running the generator
-
-### Prerequisites
+## Prerequisites
 
 You will need a [Steamship](https://steamship.com) Account and an associated API Key. With an account, an API Key can be obtained
 via the website ([https://www.steamship.com/account/api](https://www.steamship.com/account/api)).
 
 You will need to create a Replit Secret for `STEAMSHIP_API_KEY` with your Steamship API key. 
 
-### Local test/development
+## Local test/development
 
 To run the package locally, use the provided `Run` button in Replit (or open a new Shell and type `python3 api.py`).
 
-### Production deployment
+## Production deployment
 
-Once you are satisfied with your prompt, you may deploy it to Steamship in order to create running instance(s) and a demo application.
-
-First, ensure that you have descriptive package handle for your package in `steamship.json` (defaults to `my-prompt-package`). This handle will be used by Steamship to refer to your package internally and externally. It must be globally-unique, so you may need to try a few before finding one that is unclaimed.
+Once you are satisfied with your app, you may deploy it to Steamship in order to create running production-ready instance(s).
 
 To deploy it on [Steamship](https://steamship.com), open a **new** Shell and type `ship deploy`. This will push a new version of your Steamship package to Steamship's servers.
 
-Details about the package will be accessible via https://www.steamship.com/packages/<your-package-name\>. A default demo web app will also be deployed and made globally-accessible.
+Details about the package will be accessible via https://www.steamship.com/packages/<your-package-name\>.
 
 # Package Files
 
 ## `api.py`
 
-The main logic for the generator lives entirely in `api.py`. This file contains a `PromptPackage` class with one API method: `generate()`. Inside of `generate()`, the prompt is built with the user-supplied parameters and submitted to the generation plugin.
+The main logic for the generator lives entirely in `api.py`. All Steamship packages MUST have an api.py file.
 
 ## `steamship.json`
 
 After you deploy once, you will see a new `steamship.json` file in your repl. This is the manifest Steamship uses to store metadata about your package. This includes a handle for the package, as well as a version (allowing for package updates). This information will be used when you deploy your package to Steamship to present package details to users.
-
-The tag `Prompt API` signals [Steamship](https://steamship.com) to automatically generate a special web interface for your package upon instance creation (post-deployment).
 
 ## `requirements.txt`
 
@@ -64,9 +71,3 @@ allowing invocation with parameters that supports auto-scaling and persistence v
 Steamship packages can combine multiple Steamship [plugins](https://steamship.com/plugins) to build complex applications out of various AI building blocks. These plugins cover a broad variety of AI skills, including transcription, summarization, and generation.
 
 To learn more about Steamship packages and plugins, please read our docs: https://docs.steamship.com/.
-
-## Feedback and Support
-
-Have any feedback on this template? Or on [Steamship](https://steamship.com) in general?
-
-We'd love to hear from you. Please reach out to: hello@steamship.com, or join us on our [Discord](https://discord.gg/5Vry5ANVwT).
--- api.py
+++ api.py
@@ -1,6 +1,4 @@
-"""A Steamship package for prompt-based text generation.
-
-This package provides a simple template for building prompt-based applications.
+"""A Steamship package for answering questions with sources using Embeddings and LangChain.
 
 To run it:
 1. Get a Steamship API Key (Visit: https://steamship.com/account/api). If you do not
@@ -8,78 +6,154 @@ To run it:
 2. Copy this key to a Replit Secret named STEAMSHIP_API_KEY.
 3. Click the green `Run` button at the top of the window (or open a Shell and type `python3 api.py`).
 
-More information about this template is provided in README.md.
+To see additional Steamship + Langchain examples, please visit our GitHub repo:
+https://github.com/steamship-core/steamship-langchain
+
+More information is provided in README.md.
 
 To learn more about advanced uses of Steamship, read our docs at: https://docs.steamship.com/packages/using.html.
 """
-import inspect
+import logging
+from typing import Any, Dict, List
+
+import langchain
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter
+from steamship import Block, check_environment, File, RuntimeEnvironments, Steamship, Tag
+from steamship.data.plugin.index_plugin_instance import SearchResult
+from steamship.invocable import post, PackageService
+from steamship_langchain.cache import SteamshipCache
+from steamship_langchain.llms import OpenAI
 from termcolor import colored
 
-from steamship import check_environment, RuntimeEnvironments, Steamship
-from steamship.invocable import post, PackageService
 
+class QuestionAnsweringPackage(PackageService):
 
-class PromptPackage(PackageService):
-  # Modify this to customize behavior to match your needs.
-  PROMPT = "Say an unusual greeting to {name}. Compliment them on their {trait}."
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+    # set up LLM cache
+    langchain.llm_cache = SteamshipCache(self.client)
+    # set up LLM
+    self.llm = OpenAI(client=self.client,
+                      temperature=0,
+                      cache=False,
+                      max_words=250)
+    # create a persistent embedding store
+    self.index = self.client.use_plugin(
+      "embedding-index",
+      config={
+        "embedder": {
+          "plugin_handle": "openai-embedder",
+          "fetch_if_exists": True,
+          "config": {
+            "model": "text-embedding-ada-002",
+            "dimensionality": 1536,
+          }
+        }
+      },
+      fetch_if_exists=True,
+    )
 
-  # When this package is deployed, this annotation tells Steamship
-  # to expose an endpoint that accepts HTTP POST requests for the
-  # `/generate` request path.
-  # See README.md for more information about deployment.
-  @post("generate")
-  def generate(self, name: str, trait: str) -> str:
-    """Generate text from prompt parameters."""
-    llm_config = {
-      # Controls length of generated output.
-      "max_words": 30,
-      # Controls randomness of output (range: 0.0-1.0).
-      "temperature": 0.8
-    }
-    prompt_args = {"name": name, "trait": trait}
+  @post("index_file")
+  def index_file(self, file_handle: str) -> bool:
+    text_splitter = CharacterTextSplitter(chunk_size=250, chunk_overlap=0)
+    texts = []
+    file = File.get(self.client, handle=file_handle)
+    for block in file.blocks:
+      texts.extend(text_splitter.split_text(block.text))
 
-    llm = self.client.use_plugin("gpt-3", config=llm_config)
-    return llm.generate(self.PROMPT, prompt_args)
+    # give an approximate source location based on chunk size
+    items = [
+      Tag(client=self.client,
+          text=t,
+          value={"source": f"{file.handle}-offset-{i*250}"})
+      for i, t in enumerate(texts)
+    ]
+
+    self.index.insert(items)
+    return True
+
+  @post("search_embeddings")
+  def search_embeddings(self, query: str, k: int) -> List[SearchResult]:
+    """Return the `k` closest items in the embedding index."""
+    search_results = self.index.search(query, k=k)
+    search_results.wait()
+    items = search_results.output.items
+    return items
+
+  @post("/qa_with_sources")
+  def qa_with_sources(self, query: str) -> Dict[str, Any]:
+    chain = load_qa_with_sources_chain(self.llm,
+                                       chain_type="map_reduce",
+                                       verbose=False)
+    search_results = self.search_embeddings(query, k=4)
+    docs = [
+      Document(page_content=result.tag.text,
+               metadata={"source": result.tag.value.get("source", "unknown")})
+      for result in search_results
+    ]
+    return chain({"input_documents": docs, "question": query})
 
 
 # Try it out locally by running this file!
 if __name__ == "__main__":
-  print(colored("Generate Compliments with GPT-3\n", attrs=['bold']))
+  logging.getLogger().setLevel(logging.ERROR)
+  print(
+    colored("Question Answering with Sources using LangChain on Steamship\n",
+            attrs=['bold']))
 
   # This helper provides runtime API key prompting, etc.
   check_environment(RuntimeEnvironments.REPLIT)
 
+  # NOTE: we use a temporary workspace here as an example.
+  # To persist chat history across sessions, etc., use a persistent workspace.
   with Steamship.temporary_workspace() as client:
-    prompt = PromptPackage(client)
+    # when ...
+    api = QuestionAnsweringPackage(client=client)
 
-    example_name = "Han Solo"
-    example_trait = "heroism in the face of adversity"
-    print(colored("First, let's run through an example...", 'green'))
-    print(colored("Name:", 'grey'), f"{example_name}")
-    print(colored("Trait:", 'grey'), f"{example_trait}")
-    print(colored("Generating...", 'grey'))
-    print(colored("Compliment:", 'grey'),
-          f"{prompt.generate(name=example_name, trait=example_trait)}\n")
+    # Embed the State of the Union address
+    with open("state-of-the-union-2022.txt") as f:
+      print(colored(
+        "Saving the state of the union file to Steamship workspace...",
+        "blue"),
+            end="",
+            flush=True)
+      sotu_file = File.create(client, blocks=[Block(text=f.read())])
+      print(colored("Done.", "blue"))
 
-    print(colored("Now, try with your own inputs...", 'green'))
+    print(colored("Indexing state of the union...", "blue"),
+          end="",
+          flush=True)
+    # when ...
+    api.index_file(file_handle=sotu_file.handle)
+    print(colored("Done.", "blue"))
 
-    try_again = True
-    while try_again:
-      kwargs = {}
-      for parameter in inspect.signature(prompt.generate).parameters:
-        kwargs[parameter] = input(
-          colored(f'{parameter.capitalize()}: ', 'grey'))
+    # Issue Query
+    query = "tell me about Canada?"
+    print(colored("\nQuery: ", "blue"), f"{query}")
 
-      print(colored("Generating...", 'grey'))
+    print(
+      colored(
+        "Awaiting results. Please be patient. This may take a few moments.",
+        "blue"))
 
-      # This is the prompt-based generation call
-      print(colored("Compliment:", 'grey'), f'{prompt.generate(**kwargs)}\n')
+    # when...
+    response = api.qa_with_sources(query=query)
+    print(colored("Answer: ", "blue"), f"{response['output_text'].strip()}")
 
-      try_again = input(colored("Generate another (y/n)? ",
-                                'green')).lower().strip() == 'y'
-      print()
+    # Print sources (with text)
+    last_line = response['output_text'].splitlines()[-1:][0]
 
-    print("Ready to share with your friends (and the world)?")
-    print("Run ", colored("$ ship deploy ", color='green',
-                          on_color='on_black'),
-          "to get a production-ready API endpoint and web-based demo app.")
+    if "SOURCES: " not in last_line:
+      print(colored("No sources provided in response.", "red"))
+    else:
+      sources_list = last_line[len("SOURCES: "):]
+
+      for source in sources_list.split(","):
+        print(colored(f"\nSource text ({source.strip()}):", "blue"))
+        for input_doc in response['input_documents']:
+          metadata = input_doc.metadata
+          src = metadata['source']
+          if source.strip() == src:
+            print(input_doc.page_content)
